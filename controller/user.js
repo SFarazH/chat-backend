766a587dff1756eb8e4dd0ab8d54ea02f6bd6784
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-09-10 11:07:43 +0530

    add search user ft

--- controller/user.js
+++ controller/user.js
@@ -13,7 +13,7 @@ const getFriends = async (req, res) => {
 
     return res.status(200).json({
       success: true,
-      friends: populatedUser.friends, 
+      friends: populatedUser.friends,
     });
   } catch (error) {
     console.error("Error fetching friends:", error);
@@ -24,4 +24,34 @@ const getFriends = async (req, res) => {
   }
 };
 
-module.exports = { getFriends };
+const searchUsernames = async (req, res) => {
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ message: "Query parameter is required." });
+  }
+
+  try {
+    // Use a case-insensitive regular expression to find matching usernames
+    const users = await userModel
+      .find({
+        $or: [
+          { username: { $regex: query, $options: "i" } }, // Search in the username field
+          { name: { $regex: query, $options: "i" } }, // Search in the name field
+        ],
+      })
+      .select("username name");
+    if (!users.length) {
+      return res.status(404).json({ message: "No users found!" });
+    }
+
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { getFriends, searchUsernames };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { authenticate } = require("../middleware/authenticate");
-const { getFriends } = require("../controller/user");
+const { getFriends, searchUsernames } = require("../controller/user");
 
 const router = express.Router();
 
 router.get("/getFriends", authenticate, getFriends);
+router.get("/search", authenticate, searchUsernames);
 
 module.exports = router;
